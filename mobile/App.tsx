2b2861b3404b427e60a76678b4dbfd08de96cc7b
Rafael Simionato <rafaasimi@hotmail.com>
2022-11-04 15:11:08 -0300

    Finalizado aula 03

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,35 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
-import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import {THEME} from './src/styles/theme'
+import { AuthContextProvider } from "./src/context/AuthContext";
+import { THEME } from "./src/styles/theme";
 
 import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+
+import { Pools } from "./src/screens/Pools";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Pools /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
---
+++ mobile/src/screens/Find.tsx
@@ -0,0 +1,29 @@
+import { Heading, VStack } from "native-base";
+import { Header } from "../components/Header";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
+export function Find() {
+  return (
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Busque por código" showBackButton />
+
+      <VStack mt={8} mx={5} alignItems="center">
+
+        <Heading
+          fontFamily="heading"
+          color="white"
+          fontSize="xl"
+          mb={8}
+          textAlign="center"
+        >
+          Encontre um bolão através de seu código único
+        </Heading>
+
+        <Input placeholder="Qual o código do bolão?" />
+        <Button title="Buscar bolão" mt={2}/>
+
+      </VStack>
+    </VStack>
+  );
+}
